4976daed97b3cb774ba1315c5befb03e7a9df9f8
anacamargos <alcvlara@sga.pucminas.br>
2021-05-09 20:58:57 -0300

    Adding function to install app in device

--- pwa_movies/js/app.js
+++ pwa_movies/js/app.js
@@ -1,3 +1,36 @@
+const botaoInstalar = document.getElementById('btInstalar');
+let initialiseUI = function(){
+
+    botaoInstalar.removeAttribute('hidden');
+    botaoInstalar.addEventListener('click', function(){
+
+        deferredInstallPrompt.prompt();
+
+        deferredInstallPrompt.userChoice.then((choice) => {
+
+            if(choice.outcome === 'accepted'){
+
+                console.log("Usuário aceitou a instalação");
+
+            }else{
+
+                console.log("Usuário não aceitou a instalação");
+
+            }
+
+        });
+
+    });
+
+}
+
+window.addEventListener('beforeinstallprompt', gravarEvento);
+
+function gravarEvento(evt){
+    console.log("teste");
+    deferredInstallPrompt = evt;
+}
+
 // Carregamento de conteúdo
 var ajax = new XMLHttpRequest();
 ajax.open("GET", "./dados.json", true);
@@ -43,13 +76,20 @@ var card_item = function (nome, imagem, descricao, link) {
 
 //Cache conteúdo dinâmico
 var cache_cards = function(data_json){
+
     if('caches' in window){
+
         caches.delete('movie-app-v1').then(function(){
+
             console.log('Deletando cache de conteúdo antigo');
+
             if(data_json.length > 0){
+
                 var files = ['dados.json'];
+
                 //Entrando na categoria
                 for(var i = 0; i < data_json.length; i++){
+
                     //Entrando no item
                     for(var j = 0; j < data_json[i].itens.length; j++){
                         if(files.indexOf(data_json[i].itens[j].imagem) == -1){
@@ -57,12 +97,17 @@ var cache_cards = function(data_json){
                         }
                     }
                 }
+
                 caches.open('movie-app-v1').then(function (cache){
+
                     cache.addAll(files).then(function(){
                         console.log("Arquivos de conteúdo cacheados!");
                     });
+
                 });
             }
+
         });
+
     }
 }
